49556f2e5d6ef2be2340b4c9dfa56933c80ad27e
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-18 16:47:11 +0300

    Shelling

--- Basics.ps1
+++ Basics.ps1
@@ -3,5 +3,12 @@ Get-Help get-date
 Get-ExecutionPolicy
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -WhatIf
 
-$GivenName="Richard"
+$GivenName = "Richard"
 $GivenName
+
+$Value1 = 4
+$Value2 = 5
+$Value3 = 6
+
+$Result = $Value1 + $Value3
+$Result
